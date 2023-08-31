8d7faa2a6457c27b7e3714f2b5dda70d1a342fb8
EugeneDashko <eugenedashko@yahoo.com>
2023-08-31 13:20:51 +0300

    resolve coonflict with choices.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import './index.html';
 // import './page.html';
 import './index.scss';
+
 //=====================================================
 
 
 
 //new modules:
-
+const Choices = require('choices.js');
 
 //use modules
+ const choice = () => {
+    const citySelect = document.querySelector('#city');
+    const cityChoices = new Choices(citySelect, {
+        itemSelectText: '',
+        searchEnabled: false,
+
+    });
+}
+
+choice();
